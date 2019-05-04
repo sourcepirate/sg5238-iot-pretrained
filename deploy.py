055cd3fc305ff813e37d1f2aecb6ee723f94a491
sourcepirate <sathyanarrayanan@yandex.com>
2019-05-04 21:38:48 +0800

    updated deploy

--- deploy.py
+++ deploy.py
@@ -2,10 +2,6 @@ import os
 import keras2graph
 import ncs_process
 
-model_file = open("models/model.json")
-weight_file = open("models/weights.h5")
-
-
 keras2graph.keras_to_graph('models/model.json', 'input_1', 'activation_7/Softmax', 'Keras_Model/weights.h5', './graph', False)
 
 ncs_model, device = ncs_process.ready_ai_ncs('./graph', device_index=0)
